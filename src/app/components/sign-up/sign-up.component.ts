ed9e10c4eaa763c214f45b5db8bd9800dc7a4e20
Marvic Sayco <Marvicsayco@eigital.com>
2022-04-01 12:43:00 +0800

    configure signup

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
+import { QuestionService } from 'src/app/service/question.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  isEnterContestReady= false;
+  constructor(
+    private questionService:QuestionService,
+    private authService:AuthService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.questionService.setQuestionsToLocalStorage();
+  }
+    //Set user email to local storage
+  //set isEnterContestReady to true to show enterContest button
 
-  ngOnInit(): void {
+  onSubmit(signUpForm: NgForm){
+    this.isEnterContestReady = true;
+    const {emailAddress} = signUpForm.value;
+    this.authService.signUp(emailAddress);
   }
 
+  //store questions to localstorage
+  onEnterContest(){
+    this.questionService.setQuestionsToLocalStorage();
+  }
 }
