2c780805a3b8bcb13e1ab223c6e30fd446e06ccf
Marvic Sayco <Marvicsayco@eigital.com>
2022-04-01 12:28:10 +0800

    1 generate  components,mode,service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
